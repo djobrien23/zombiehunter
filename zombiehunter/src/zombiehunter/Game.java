ab369a45f1edfaa3033a7a405dc70360ac1d2cf1
djobrien <djobrien17283@hawkmail.hfcc.edu>
2016-11-29 17:58:34 -0500

    added rectangle draw, zombie update, player shoot, and projectile list

--- zombiehunter/src/zombiehunter/Game.java
+++ zombiehunter/src/zombiehunter/Game.java
@@ -7,6 +7,10 @@ import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.*;
+import java.util.ArrayList;
 
 // Actual game
 public class Game {
@@ -25,14 +29,27 @@ public class Game {
             @Override
             public void run(){
                 // Sets variables and objects for the game.
+        
                 Initialize();
                 // Load game files (images, sounds, ...)
                 LoadContent();
                 
                 Framework.gameState = Framework.GameState.PLAYING;
+                
+                        ArrayList projectiles = player.getProjectiles();
+			for (int i = 0; i < projectiles.size(); i++) 
+         {
+				Projectile p = (Projectile) projectiles.get(i);
+				if (p.isVisible() == true) {
+					p.update();
+				} else {
+					projectiles.remove(i);
+				}
+			}
             }
         };
         threadForInitGame.start();
+        
     }
     
     
@@ -66,6 +83,10 @@ public class Game {
     public void UpdateGame(long gameTime)
     {
         player.Update(); // move the player
+        if(Canvas.keyboardKeyState(KeyEvent.VK_SPACE))
+         player.shoot();
+         
+        zombie.Update(); // move zombie
     }
     
     // draw the game to the screen
@@ -73,11 +94,19 @@ public class Game {
     {
         g2d.drawImage(backgroundImg, 0, 0, Framework.frameWidth, Framework.frameHeight, null);
         
+        g2d.drawRect((int)zombie.rect2.getX(), (int)zombie.rect2.getY(), (int)zombie.rect2.getWidth(), (int)zombie.rect2.getHeight());
         zombie.Draw(g2d);
         
+        g2d.drawRect((int)player.rect.getX(), (int)player.rect.getY(), (int)player.rect.getWidth(), (int)player.rect.getHeight());
         player.Draw(g2d);
+        
+        ArrayList projectiles = player.getProjectiles();
+		for (int i = 0; i < projectiles.size(); i++) {
+			Projectile p = (Projectile) projectiles.get(i);
+			g2d.setColor(Color.BLACK);
+			g2d.fillRect(p.getX(), p.getY(), 10, 5);
+          }
     }
-    
     
     // draw the game over screen
     public void DrawGameOver(Graphics2D g2d, long gameTime)
@@ -86,5 +115,6 @@ public class Game {
         
         g2d.drawString("Press space or enter to restart.", Framework.frameWidth / 2 - 100, Framework.frameHeight / 3 + 70);
 
-    }
+    }   
+	
 }
